8206a3214d60f03929db85aeea33d475a290494d
alkrivosheev <alkrivosheev@gmail.com>
2023-11-21 11:49:09 +0300

    Init

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -9,22 +9,21 @@ import org.junit.jupiter.api.Test;
  * Unit test for simple App.
  */
 public class AppTest
-        extends TestCase
-{
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
     public AppTest( String testName ) {
-        super( testName );
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
     public static Test suite() {
-        return new TestSuite( AppTest.class );
+        return new TestSuite(AppTest.class);
     }
 
     /**
