ad228183b9e50be91381c0cdfab9b6514b82e08b
alkrivosheev <alkrivosheev@gmail.com>
2023-12-12 13:45:17 +0300

    Balancer

---
+++ src/main/java/ru/job4j/iterator/Balancer.java
@@ -0,0 +1,18 @@
+package ru.job4j.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Balancer {
+    public static void split(List<ArrayList<Integer>> nodes, Iterator<Integer> source) {
+        while (source.hasNext()) {
+            for (List node : nodes) {
+                if (!source.hasNext()) {
+                    break;
+                }
+                node.add(source.next());
+            }
+        }
+    }
+}
