d0fb7ac603c02ea918dd8227439934608b8cf94d
alkrivosheev <alkrivosheev@gmail.com>
2024-03-21 07:34:42 +0300

    SimpleTree

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,9 +1,30 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            Node<E> current = queue.poll();
+            if (condition.test(current)) {
+                return Optional.of(current);
+            }
+            queue.addAll(current.children);
+        }
+
+        return Optional.empty();
+    }
+
+    public boolean isBinary() {
+        Predicate<Node<E>> moreThanTwoChildren = node -> node.children.size() > 2;
+        return !findByPredicate(moreThanTwoChildren).isPresent();
+    }
 
     public SimpleTree(final E root) {
         this.root = new Node<>(root);
@@ -11,15 +32,14 @@ public class SimpleTree<E> implements Tree<E> {
 
     @Override
     public boolean add(E parent, E child) {
-        boolean result = false;
-        Optional<Node<E>> parentNodeOptional = findBy(parent);
-        Optional<Node<E>> childNodeOptional = findBy(child);
-        if (parentNodeOptional.isPresent() && childNodeOptional.isEmpty()) {
-            Node<E> parentNode = parentNodeOptional.get();
+        Predicate<Node<E>> findParent = node -> node.value.equals(parent);
+        Predicate<Node<E>> findChild = node -> node.value.equals(child);
+        if (findByPredicate(findParent).isPresent() && !findByPredicate(findChild).isPresent()) {
+            Node<E> parentNode = findByPredicate(findParent).get();
             parentNode.children.add(new Node<>(child));
-            result = true;
+            return true;
         }
-        return result;
+        return false;
     }
 
     @Override
