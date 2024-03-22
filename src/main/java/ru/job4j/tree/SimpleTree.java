e295d4ec37c0135d7ca27baf89addad47189ed43
alkrivosheev <alkrivosheev@gmail.com>
2024-03-22 06:43:23 +0300

    SimpleTree

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -6,55 +6,45 @@ import java.util.function.Predicate;
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
 
-    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
-        Queue<Node<E>> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            Node<E> current = queue.poll();
-            if (condition.test(current)) {
-                return Optional.of(current);
-            }
-            queue.addAll(current.children);
-        }
-
-        return Optional.empty();
-    }
-
-    public boolean isBinary() {
-        Predicate<Node<E>> moreThanTwoChildren = node -> node.children.size() > 2;
-        return !findByPredicate(moreThanTwoChildren).isPresent();
-    }
-
     public SimpleTree(final E root) {
         this.root = new Node<>(root);
     }
 
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
+        Optional<Node<E>> result = Optional.empty();
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            Node<E> current = queue.poll();
+            if (condition.test(current)) {
+                result =  Optional.of(current);
+            }
+            queue.addAll(current.children);
+        }
+        return result;
+    }
+
+    public boolean isBinary() {
+        Predicate<Node<E>> moreThanTwoChildren = node -> node.children.size() > 2;
+        return findByPredicate(moreThanTwoChildren).isEmpty();
+    }
+
     @Override
     public boolean add(E parent, E child) {
+        boolean result = false;
         Predicate<Node<E>> findParent = node -> node.value.equals(parent);
         Predicate<Node<E>> findChild = node -> node.value.equals(child);
-        if (findByPredicate(findParent).isPresent() && !findByPredicate(findChild).isPresent()) {
+        if (findByPredicate(findParent).isPresent() && findByPredicate(findChild).isEmpty()) {
             Node<E> parentNode = findByPredicate(findParent).get();
             parentNode.children.add(new Node<>(child));
-            return true;
+            result = true;
         }
-        return false;
+        return result;
     }
 
     @Override
     public Optional<Node<E>> findBy(E value) {
-        Optional<Node<E>> result = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> element = data.poll();
-            if (element.value.equals(value)) {
-                result = Optional.of(element);
-                break;
-            }
-            data.addAll(element.children);
-        }
-        return result;
+        Predicate<Node<E>> findElement = node -> node.value.equals(value);
+        return findByPredicate(findElement);
     }
 }
