c4d909dd5aae985e61c22e6fa1d6a5a9b2fdeb15
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2024-04-06 00:13:09 +0300

    LogFilter

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -17,7 +17,7 @@ public class LogFilter {
         try (BufferedReader input = new BufferedReader(new FileReader(this.file))) {
             for (String line = input.readLine(); line != null; line = input.readLine()) {
                 String[] words = line.split(" ");
-                if (words[words.length - 2].equals("404")) {
+                if ("404".equals(words[words.length - 2])) {
                     res.add(line);
                 }
             }
