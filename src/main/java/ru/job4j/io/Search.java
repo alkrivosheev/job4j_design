a18cbbb6cd349ae40b34660f23b18e6ea69efcce
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2024-04-11 13:42:51 +0300

    args

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -5,12 +5,24 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (!validate(args)) {
+            throw new IllegalArgumentException("Parameters are not specified. Usage: ROOT_FOLDER file_extension");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    public static boolean validate(String[] args) {
+        boolean res = false;
+        if (args.length == 2 && !Objects.equals(args[0], "") && !Objects.equals(args[1], "")) {
+            res = true;
+        }
+        return res;
     }
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
