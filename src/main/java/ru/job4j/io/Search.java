18dda628af6858edbd8113410deb07fe970a085e
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2024-04-11 15:44:23 +0300

    args

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -5,23 +5,28 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        if (!validate(args)) {
-            throw new IllegalArgumentException("Parameters are not specified. Usage: ROOT_FOLDER file_extension");
+        if (validate(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
         }
-        Path start = Paths.get(args[0]);
-        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static boolean validate(String[] args) {
-        boolean res = false;
-        if (args.length == 2 && !Objects.equals(args[0], "") && !Objects.equals(args[1], "")) {
+        boolean res;
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Parameters are not specified. Usage: ROOT_FOLDER file_extension");
+        } else if (!Files.exists(Path.of(args[0]))) {
+            throw new IllegalArgumentException("Use folder name for search. Usage: ' . ' or ' C:\\' ");
+        } else if (!args[1].matches("\\.\\w+$")) {
+            throw new IllegalArgumentException("Set file extension. Usage: '.exe' or '.js'");
+        }else {
             res = true;
         }
+
         return res;
     }
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
