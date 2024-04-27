171eccf51ee43a15221f663134b9b42d289d0019
alkrivosheev <alkrivosheev@gmail.com>
2024-04-27 11:31:10 +0300

    DatFileIO

---
+++ src/main/java/ru/job4j/io/RandomAccess.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class RandomAccess {
+    public static void main(String[] args)  {
+        try {
+            RandomAccessFile randomAccess = new RandomAccessFile("data/random.txt", "rw");
+            randomAccess.writeInt(100);
+
+            randomAccess.writeBoolean(true);
+            randomAccess.writeChar('А');
+            randomAccess.seek(0);
+            System.out.println(randomAccess.readInt());
+            System.out.println(randomAccess.readBoolean());
+            System.out.println(randomAccess.readChar());
+            randomAccess.seek(5);
+            System.out.println(randomAccess.readChar());
+            System.out.println(randomAccess.getFilePointer());
+            randomAccess.seek(5);
+            randomAccess.writeChar('B');
+            randomAccess.seek(5);
+            System.out.println(randomAccess.readChar());
+            randomAccess.seek(randomAccess.length());
+            System.out.println("Положение указателя после 'B': " + randomAccess.getFilePointer());
+            randomAccess.writeDouble(3.01);
+            randomAccess.seek(7);
+            System.out.println(randomAccess.readDouble());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
