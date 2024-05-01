f6b9dd25b71ae5c08169668ba469d5379ea98f95
alkrivosheev <68105349+alkrivosheev@users.noreply.github.com>
2024-05-01 19:57:46 +0300

    log4j

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,15 +7,10 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        byte btn = 8;
-        short srt = 13;
-        int age = 31;
-        long length = 999888779999887899L;
-        float sqrt = 17.25F;
-        double rad = 3.15;
-        boolean state = true;
-        char name = 'h';
-        LOG.debug("User info name : {}, age : {}, btn : {}, srt : {}, length : {}, sqrt : {}, rad : {}, state : {}",
-                name, age, btn, srt, length, sqrt, rad, state);
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Error", e);
+        }
     }
 }
