f0f8f3858b01fdd11520725b9988fb897248c27b
alkrivosheev <alkrivosheev@gmail.com>
2024-05-01 10:53:50 +0300

    slf4j

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,15 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte btn = 8;
+        short srt = 13;
+        int age = 31;
+        long length = 999888779999887899L;
+        float sqrt = 17.25F;
+        double rad = 3.15;
+        boolean state = true;
+        char name = 'h';
+        LOG.debug("User info name : {}, age : {}, btn : {}, srt : {}, length : {}, sqrt : {}, rad : {}, state : {}",
+                name, age, btn, srt, length, sqrt, rad, state);
     }
 }
